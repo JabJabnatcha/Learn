7b17d34383715b3210d9b30eb40e373b8b46b3b2
JabJab Natcha <natcharapaphon@gmail.com>
2025-07-30 22:44:15 +0700

    add db context

---
+++ J_Messenger/Models/DbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace J_Messenger.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Conversation> Conversations { get; set; }
+    }
+}
