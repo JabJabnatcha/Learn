4da3862a7c3f32cce2901b67bae5bd7af4b760a8
JabJab Natcha <natcharapaphon@gmail.com>
2026-01-14 20:46:43 +0700

    save race sub race and class and subclass 1

--- React_DnD/react-dnd/src/data/classes.js
+++ React_DnD/react-dnd/src/data/classes.js
@@ -1,6 +1,175 @@
 export const CLASSES = {
+    Barbarian: {
+        base:{
+            PrimaryStat: "Strength",
+            HitDie: 12,
+            SavingThrows: ["Strength", "Constitution"],
+            SkillChoices:{ 
+                from: [
+                    "Animal Handling", 
+                    "Athletics", 
+                    "Intimidation", 
+                    "Nature", "Perception", 
+                    "Survival"],
+                choose: 2 
+            },
+            ArmorProficiencies: ["Light Armor", "Medium Armor"],
+            WeaponProficiencies: ["Simple Weapons", "Martial Weapons"],
+            startingEquipment: {
+                A: ["Greataxe", "Any Martial Melee Weapon"],
+                B: ["Two Handaxes", "Any Simple Weapon"],
+                C: ["Explorer's Pack", "Javelin (4)"]
+            }
+        },
+        subClasses: ["Berserker", "Totem Warrior"]
+    },
+    Bard: {
+        base:{
+            PrimaryStat: "Charisma",
+            HitDie: 8,
+            SavingThrows: ["Dexterity", "Charisma"],
+            SkillChoices:{
+                from: [
+                    "Acrobatics", 
+                    "Animal Handling", 
+                    "Arcana",
+                    "Athletics",
+                    "Deception",
+                    "History",
+                    "Insight",
+                    "Intimidation",
+                    "Investigation",
+                    "Medicine",
+                    "Nature",
+                    "Perception",
+                    "Performance",
+                    "Persuasion",
+                    "Religion",
+                    "Sleight of Hand",
+                    "Stealth",
+                    "Survival"
+                ],
+                choose: 3 
+            },
+            ArmorProficiencies: ["Light Armor"],
+            WeaponProficiencies: ["Simple Weapons"," Longswords", "Rapiers", "Shortswords", "Hand Crossbows"],
+            startingEquipment: {
+                A: ["Rapier", "Longsword", "Any Simple Weapon"],
+                B: ["Diplomat's Pack", "Entertainer's Pack"],
+                C: ["Lute", "Any Other Musical Instrument" ]
+            }
+        },
+        subClasses: ["College of Lore", "College of Valor"]
+    },
+    Cleric: {
+        base:{
+            PrimaryStat: "Wisdom",
+            HitDie: 8,
+            SavingThrows: ["Wisdom", "Strength"],
+            SkillChoices:{
+                from: [
+                    "History",
+                    "Insight",
+                    "Medicine",
+                    "Persuasion",
+                    "Religion"
+                ],
+                choose: 2 
+            },
+            ArmorProficiencies: ["Light Armor", "Medium Armor", "Shields"],
+            WeaponProficiencies: ["Simple Weapons"],
+            startingEquipment: {
+                A: ["Mace", "Warhammer (if proficient)"],
+                B: ["Scale Mail", "Leather Armor", "Chain Mail (if proficient)"],
+                C: ["Light Crossbow (20 bolts)", "Any Simple Weapon"],
+                D: ["Priest's Pack", "Explorer's Pack"],
+                E: ["Shield", "Holy Symbol"]
+            }
+        },
+        subClasses: ["Life Domain", "Light Domain"]
+    },
+    Druid: {
+        base:{
+            PrimaryStat: "Wisdom",
+            HitDie: 8,
+            SavingThrows: ["Wisdom", "Dexterity"],
+            SkillChoices:{
+                from: [
+                    "Animal Handling",
+                    "Arcana",
+                    "Insight",
+                    "Medicine",
+                    "Nature",
+                    "Perception",
+                    "Religion",
+                    "Survival"
+                ],
+                choose: 2 
+            },
+            ArmorProficiencies: ["Light Armor", "Shields (non-metal)"],
+            WeaponProficiencies: ["Club", "Dagger", "Dart", "Javelin", "Mace", "Quarterstaff", "Scimitar", "Sickle", "Sling", "Spear"],
+            startingEquipment: {
+                A: ["Wooden Shield", "Any Simple Weapon"],
+                B: ["Scimitar", "Any Simple Melee Weapon"],
+                C: ["Leather Armor", "Druidic Pack", "Druidic Focus"]
+            }
+        },
+        subClasses: ["Circle of the Land", "Circle of the Moon"]
+    },
     Fighter: {
+        base:{
+            PrimaryStat: "Strength or Dexterity",
+            HitDie: 10,
+            SavingThrows: ["Strength", "Constitution"],
+            SkillChoices:{
+                from: [
+                    "Acrobatics", 
+                    "Animal Handling", 
+                    "Athletics",
+                    "History",
+                    "Insight",
+                    "Intimidation",
+                    "Perception",
+                    "Survival"
+                ],
+                choose: 2 
+            },
+            ArmorProficiencies: ["Light Armor", "Medium Armor", "Heavy Armor", "Shields"],
+            WeaponProficiencies: ["Simple Weapons", "Martial Weapons"],
+            startingEquipment: {
+                A: ["Chain Mail", "Leather Armor, Longbow (20 arrows)"],
+                B: ["Any Martial Weapon and a Shield", "Two Martial Weapons"],
+                C: ["Light Crossbow (20 bolts) or Two Handaxes", "Any Simple Weapon"],
+                D: ["Dungeoneer's Pack", "Explorer's Pack"]
+            }
+        },
         subClasses: ["Champion", "Battle Master"]
+    },
+    Monk: {
+        base:{
+            PrimaryStat: "Dexterity and Wisdom",
+            HitDie: 8,
+            SavingThrows: ["Strength", "Dexterity"],
+            SkillChoices:{
+                from: [
+                    "Acrobatics",
+                    "Athletics",
+                    "History",
+                    "Insight",
+                    "Religion",
+                    "Stealth"
+                ],
+                choose: 2 
+            },
+            ArmorProficiencies: [],
+            WeaponProficiencies: ["Simple Weapons", "Shortswords"],
+            startingEquipment: {
+                A: ["Shortsword", "Any Simple Weapon"],
+                B: ["Dungeoneer's Pack", "Explorer's Pack"],
+                C: ["10 Darts"]
+            }
+        },
+        subClasses: ["Way of the Open Hand", "Way of Shadow"]
     },
     Wizard: {
         subClasses: ["Evocation", "Necromancy"]
--- React_DnD/react-dnd/src/data/races.js
+++ React_DnD/react-dnd/src/data/races.js
@@ -1,11 +1,132 @@
 export const RACES = {
-    Human: {
-        subRaces: []
+    Dragonborn: {
+        base: {
+            features: ["Draconic Ancestry", "Breath Weapon", "Damage Resistance","Darkvision","Draconic Flight"],
+            bonus: { Strength: 2, Charisma: 1 },
+            speed: 30,
+            languages: ["Common", "Draconic"]
+        },
+        subRaces: [],
+    },
+
+    Dwarf: {
+        base: {
+            features: ["Darkvision", "Dwarven Resilience", "Stonecunning"],
+            bonus: { Constitution: 2 },
+            speed: 30,
+            languages: ["Common", "Dwarvish"],
+        },
+        subRaces: {
+            "Hill Dwarf": {
+                features: ["Dwarven Toughness"],
+                bonus: { Wisdom: 1 }
+            },
+            "Mountain Dwarf": {
+                features: ["Dwarven Armor Training"],
+                bonus: { Strength: 2 }
+            }
+        }
     },
     Elf: {
-        subRaces: ["High Elf", "Wood Elf", "Dark Elf"]
+        base: {
+            features: ["Darkvision", "Fey Ancestry", "Trance", "Keen Senses"],
+            bonus: { Dexterity: 2 },
+            speed: 30,
+            languages: ["Common", "Elvish"],
+        },
+        subRaces: {
+            "High Elf": {
+                features: ["Extra Language", "Cantrip"],
+                bonus: { Intelligence: 1 }
+            },
+            "Wood Elf": {
+                features: ["Fleet of Foot", "Mask of the Wild"],
+                bonus: { Wisdom: 1 },
+                speed: 35
+            },
+            "Dark Elf (Drow)": {
+                features: ["Superior Darkvision", "Sunlight Sensitivity", "Drow Magic"],
+                bonus: { Charisma: 1 }
+            }
+        }
     },
-    Dwarf: {
-        subRaces: ["Hill Dwarf", "Mountain Dwarf"]
-    }
+    // Gnome: {
+    //     subRaces: ["Forest Gnome", "Rock Gnome"],
+    //     features: ["Darkvision", "Gnome Cunning"],
+    //     bonus: { Intelligence: 2 }
+    // },
+    // Goliath: {
+    //     subRaces: [],
+    //     features: ["Stone's Endurance", "Powerful Build"],
+    //     bonus: { Strength: 2, Constitution: 1 }
+    // },
+    Halfling: {
+        base: {
+            features: ["Lucky", "Brave", "Halfling Nimbleness"],
+            bonus: { Dexterity: 2 },
+            speed: 25,
+            languages: ["Common", "Halfling"],
+        },
+        subRaces: {
+            "Lightfoot": {
+                features: ["Naturally Stealthy"],
+                bonus: { Charisma: 1 }
+            },
+            "Stout": {
+                features: ["Stout Resilience"],
+                bonus: { Constitution: 1 }
+            }
+        }
+    },
+    Human: {
+        base:{
+            features: ["Versatile", "Extra Language" , "Skillful" , "Adaptable" ,"Resouceful"],
+            bonus: { Strength: 1, Dexterity: 1, Constitution: 1, Intelligence: 1, Wisdom: 1, Charisma: 1 },
+            speed: 30,
+            languages: ["Common", "One extra language of your choice"] //เพิ่มเติมได้ 1ภาษา
+        },
+        subRaces: [],
+    },
+    Orc: {
+        base:{
+            features: ["Darkvision", "Aggressive", "Adrenaline Rush", "Relentless Endurance"],
+            bonus: { Strength: 2, Constitution: 1 },
+            speed: 30,
+            languages: ["Common", "Orc"]
+        },
+        subRaces: [],
+    },
+    Tiefling: {
+        base: {
+            features: ["Darkvision", "Hellish Resistance"],
+            bonus: { Intelligence: 1, Charisma: 2 },
+            speed: 30,
+            languages: ["Common", "Infernal"],
+        },
+        subRaces: {
+            "Asmodeus Tiefling": {
+                features: ["Legacy of Asmodeus"],
+                bonus: {}
+            },
+            "Chthonic Tiefling": {
+                features: ["Legacy of the Chthonic"],
+                bonus: {}
+            },
+            "Infernal Tiefling": {
+                features: ["Legacy of the Infernal"],
+                bonus: {}
+            }
+        }
+    },
+    
+    // HalfElf: {
+    //     subRaces: [],
+    //     features: ["Darkvision", "Fey Ancestry", "Skill Versatility"],
+    //     bonus: { Charisma: 2 }
+    // },
+    // HalfOrc: {
+    //     subRaces: [],
+    //     features: ["Darkvision", "Relentless Endurance", "Savage Attacks"],
+    //     bonus: { Strength: 2, Constitution: 1 }
+    // },
 };
